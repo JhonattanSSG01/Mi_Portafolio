a8036fdd11467cb57677c74894765f95ee65c994
Jhonattan Saavedra Gomez <jhonattanestivensaavedragomez@gmail.com>
2023-01-12 22:18:34 -0500

    Agregando about me

--- src/pages/Header.jsx
+++ src/pages/Header.jsx
@@ -86,7 +86,13 @@ export const Header = () => {
         <hr className="lineBlue line" />
         <hr className="lineLigthBlue line" />
 
-        {/* <img src={About} alt="Imagen alusiva a mi" width={'100%'} /> */}
+        <div>
+          <h1 className='titlePrincipal'>About Me <span></span></h1>
+          <img className='mt-3 mb-3' src={About} alt="Imagen alusiva a mi" width={'100%'} style={{opacity: '50%'}} />
+          <p className='aboutDesc p-3'>
+            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries
+          </p>
+        </div>
       </div>
     </>
   );
