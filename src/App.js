fac0e60ac4815ee8d709b2e1b10f7bb863a7fdaf
Jhonattan Saavedra Gomez <jhonattanestivensaavedragomez@gmail.com>
2023-01-12 22:05:49 -0500

    Agregando header y banner en mobile

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 
 import './App.css';
-import Header from './pages/Header';
+import { Header } from './pages/Header';
 
 function App() {
   return (
-    <Header/>
+    <Header />
   );
 }
 
--- src/pages/Header.jsx
+++ src/pages/Header.jsx
@@ -1,4 +1,8 @@
+import './styles.css';
 import Logo from './assets/LogoHeader.png';
+import Background from './assets/banner.jpg';
+import About from './assets/aboutMe.png';
+import Contact from './assets/contact.png';
 import React, { useState } from "react";
 import {
   Collapse,
@@ -8,9 +12,15 @@ import {
   Nav,
   NavItem,
   NavLink,
+  Card,
+  CardImg,
+  CardImgOverlay,
+  CardTitle,
+  CardText,
+  Button,
 } from "reactstrap";
 
-function Header(props) {
+function NavHeader(props) {
   const [collapsed, setCollapsed] = useState(true);
 
   const toggleNavbar = () => setCollapsed(!collapsed);
@@ -19,18 +29,25 @@ function Header(props) {
     <div>
       <Navbar color="faded" light>
         <NavbarBrand href="/" className="me-auto">
-          <img src={Logo} alt='Logo personal' style={{width: '100px'}}/>
+          <img src={Logo} alt="Logo personal" style={{ width: "100px" }} />
         </NavbarBrand>
         <NavbarToggler onClick={toggleNavbar} className="me-2" />
         <Collapse isOpen={!collapsed} navbar>
-          <Nav navbar>
+          <Nav navbar style={{ textAlign: "center" }}>
             <NavItem>
-              <NavLink href="/components/">Components</NavLink>
+              <NavLink>About</NavLink>
             </NavItem>
             <NavItem>
-              <NavLink href="https://github.com/reactstrap/reactstrap">
-                GitHub
-              </NavLink>
+              <NavLink>Skills</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink>Projects</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink>Contact</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink style={{width: 80, margin: 'auto', border: '3px solid var(--colorSecondary)', borderRadius: 5 }}>Resume</NavLink>
             </NavItem>
           </Nav>
         </Collapse>
@@ -39,4 +56,38 @@ function Header(props) {
   );
 }
 
-export default Header;
+export default NavHeader;
+
+
+export const Header = () => {
+  return (
+    <>
+      <NavHeader />
+      <div className="container p-0">
+        <Card className="containerBanner" inverse>
+          <CardImg className="banner" alt="Card image cap" src={Background} />
+          <CardImgOverlay className="containerText pt-5 pb-5 text-center">
+            <CardTitle tag="h3" className="titleHi sizeText">
+              Hola Mundo, my name is <span>Jhonattan Saavedra Gómez</span>
+            </CardTitle>
+            <CardText tag="h3" className="titleMuted mb-4">
+              <small>I turn people's ideas into web creativity.</small>
+            </CardText>
+            <CardText className="description sizeText">
+              I'm a growing FullStack Web Developer focused on front-end but
+              with basic knowledge in Back-end. I’m passionate about technology
+              and enjoy being able to build things based on imaginative ideas
+              through creativity.
+            </CardText>
+            <NavLink className="button">See More</NavLink>
+          </CardImgOverlay>
+        </Card>
+
+        <hr className="lineBlue line" />
+        <hr className="lineLigthBlue line" />
+
+        {/* <img src={About} alt="Imagen alusiva a mi" width={'100%'} /> */}
+      </div>
+    </>
+  );
+}
