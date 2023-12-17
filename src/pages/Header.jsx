5605ee247158242f78b05f7c0f378c61d2fcf3ad
Jhonattan Saavedra Gomez <jhonattanestivensaavedragomez@gmail.com>
2023-12-17 18:29:57 -0500

    Agregando nuevo CV

--- src/pages/Header.jsx
+++ src/pages/Header.jsx
@@ -52,7 +52,7 @@ export default function Header() {
             <NavItem className="navItem">
               <NavLink
                 className="resume animate__animated animate__wobble"
-                href="https://drive.google.com/file/d/1UcNYQdz6G4cbkeHdqLLb3DildzpfxTcy/view?usp=sharing"
+                href="https://drive.google.com/file/d/1YBa2pdavsoaSsBlZtzEEneD1m4xmKIWG/view?usp=sharing"
                 target="_blank"
               >
                 Hoja de Vida
